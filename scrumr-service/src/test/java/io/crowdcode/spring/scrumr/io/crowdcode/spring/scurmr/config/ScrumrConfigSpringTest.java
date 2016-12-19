34f3ea43ec1ff847fa1cd1805520724e927929f6
Ingo Düppe <ingo.dueppe@crowdcode.io>
2016-12-19 16:22:39 +0100

    Solution 3

---
+++ scrumr-service/src/test/java/io/crowdcode/spring/scrumr/io/crowdcode/spring/scurmr/config/ScrumrConfigSpringTest.java
@@ -0,0 +1,28 @@
+package io.crowdcode.spring.scrumr.io.crowdcode.spring.scurmr.config;
+
+import io.crowdcode.spring.scrumr.ScrumrApplication;
+import io.crowdcode.spring.scrumr.service.UserManagementService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Ingo Düppe (Crowdcode)
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes={ScrumrConfig.class})
+@ContextConfiguration(classes={ScrumrApplication.class})
+public class ScrumrConfigSpringTest {
+
+    @Autowired
+    private UserManagementService bean;
+
+    @Test
+    public void testBean() throws Exception {
+        assertNotNull(bean);
+    }
+}
---
+++ scrumr-service/src/test/java/io/crowdcode/spring/scrumr/io/crowdcode/spring/scurmr/config/ScrumrConfigTest.java
@@ -0,0 +1,44 @@
+package io.crowdcode.spring.scrumr.io.crowdcode.spring.scurmr.config;
+
+import io.crowdcode.spring.scrumr.service.UserManagementService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Ingo Düppe (Crowdcode)
+ */
+public class ScrumrConfigTest {
+
+//    @Autowired
+//    private ApplicationContext applicationContext;
+
+    @Test
+    public void testSpringContext() throws Exception {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ScrumrConfig.class);
+
+        UserManagementService bean = context.getBean(UserManagementService.class);
+
+        for (String name : context.getBeanDefinitionNames()) {
+            System.out.println(">"+name);
+        }
+
+
+        Arrays.stream(context
+                .getBeanDefinitionNames())
+                    .forEach(n -> {
+                        String type = context.getType(n).getCanonicalName();
+                        System.out.println(">>>> "+n+":"+type);
+
+                    });
+
+        assertNotNull(bean);
+    }
+
+
+}
